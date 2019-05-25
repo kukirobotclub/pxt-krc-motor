d6dc05d6586d0ea4973813ae0cfc9cb4e6f77c3d
kukirobotclub <50830118+kukirobotclub@users.noreply.github.com>
2019-05-25 16:24:07 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,178 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+
+namespace KRCmotor {
+    /**
+     * The user selects the 4-way dc motor.
+     */
+    export enum Motors {
+        M1 = 0x1,
+        M2 = 0x2,
+        M3 = 0x3,
+        M4 = 0x4
+    }
+
+    /**
+     * The user defines the motor rotation direction.
+     */
+    export enum Dir {
+        STOP = 0,
+        FWD = 1,
+        REV = 2
+    }
+
+    let pwm1init = false
+    let pwm2init = false
+    let pwm3init = false
+    let pwm4init = false
+
+    function initPwm(index: Motors): void {
+        if (index == 1) {
+            pins.analogSetPeriod(AnalogPin.P8, 20)	//50KHz
+            pins.analogSetPeriod(AnalogPin.P12, 20)	//50KHz
+            let pwm1init = true
+        }
+        if (index == 2) {
+            pins.analogSetPeriod(AnalogPin.P13, 20)	//50KHz
+            pins.analogSetPeriod(AnalogPin.P14, 20)	//50KHz
+            let pwm2init = true
+        }
+        if (index == 3) {
+            pins.analogSetPeriod(AnalogPin.P15, 20)	//50KHz
+            pins.analogSetPeriod(AnalogPin.P16, 20)	//50KHz
+            let pwm3init = true
+        }
+        if (index == 4) {
+            pins.analogSetPeriod(AnalogPin.P1, 20)	//50KHz
+            pins.analogSetPeriod(AnalogPin.P2, 20)	//50KHz
+            let pwm4init = true
+        }
+    }
+
+    //% weight=90
+    //% blockId=motor_MotorSpeed block="Motor|%index|dir|%Dir|speed|%speed"
+    //% speed.min=0 speed.max=1023
+    //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2
+    //% direction.fieldEditor="gridpicker" direction.fieldOptions.columns=2
+    export function MotorSpeed(index: Motors, direction: Dir, speed: number): void {
+        if (speed >= 1024) {
+            speed = 1023
+        }
+        if (speed < 0) {
+            speed = 0
+        }
+        if (index == 1) {	//Motor1
+            if (!pwm1init) {
+                initPwm(1)
+            }
+            if (direction == 1) {
+                pins.analogWritePin(AnalogPin.P8, speed)
+                pins.digitalWritePin(DigitalPin.P12, 0)
+            } else if (direction == 2) {
+                pins.digitalWritePin(DigitalPin.P8, 0)
+                pins.analogWritePin(AnalogPin.P12, speed)
+            } else {
+                pins.digitalWritePin(DigitalPin.P8, 0)
+                pins.digitalWritePin(DigitalPin.P12, 0)
+            }
+        }
+        if (index == 2) {	//Motor2
+            if (!pwm2init) {
+                initPwm(2)
+            }
+            if (direction == 1) {
+                pins.analogWritePin(AnalogPin.P13, speed)
+                pins.digitalWritePin(DigitalPin.P14, 0)
+            } else if (direction == 2) {
+                pins.digitalWritePin(DigitalPin.P13, 0)
+                pins.analogWritePin(AnalogPin.P14, speed)
+            } else {
+                pins.digitalWritePin(DigitalPin.P13, 0)
+                pins.digitalWritePin(DigitalPin.P14, 0)
+            }
+        }
+        if (index == 3) {	//Motor3
+            if (!pwm3init) {
+                initPwm(3)
+            }
+            if (direction == 1) {
+                pins.analogWritePin(AnalogPin.P15, speed)
+                pins.digitalWritePin(DigitalPin.P16, 0)
+            } else if (direction == 2) {
+                pins.digitalWritePin(DigitalPin.P15, 0)
+                pins.analogWritePin(AnalogPin.P16, speed)
+            } else {
+                pins.digitalWritePin(DigitalPin.P15, 0)
+                pins.digitalWritePin(DigitalPin.P16, 0)
+            }
+        }
+        if (index == 4) {	//Motor4
+            if (!pwm4init) {
+                initPwm(4)
+            }
+            if (direction == 1) {
+                pins.analogWritePin(AnalogPin.P1, speed)
+                pins.digitalWritePin(DigitalPin.P2, 0)
+            } else if (direction == 2) {
+                pins.digitalWritePin(DigitalPin.P1, 0)
+                pins.analogWritePin(AnalogPin.P2, speed)
+            } else {
+                pins.digitalWritePin(DigitalPin.P1, 0)
+                pins.digitalWritePin(DigitalPin.P2, 0)
+            }
+        }
+    }
+
+    //% weight=90
+    //% blockId=motor_MotorOnOff block="Motor|%index|dir|%Dir"
+    //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2
+    //% direction.fieldEditor="gridpicker" direction.fieldOptions.columns=2
+    export function MotorOnOff(index: Motors, direction: Dir): void {
+        if (index == 1) {	//Motor1
+            if (direction == 1) {
+                pins.digitalWritePin(DigitalPin.P8, 1)
+                pins.digitalWritePin(DigitalPin.P12, 0)
+            } else if (direction == 2) {
+                pins.digitalWritePin(DigitalPin.P8, 0)
+                pins.digitalWritePin(DigitalPin.P12, 1)
+            } else {
+                pins.digitalWritePin(DigitalPin.P8, 0)
+                pins.digitalWritePin(DigitalPin.P12, 0)
+            }
+        }
+        if (index == 2) {	//Motor2
+            if (direction == 1) {
+                pins.digitalWritePin(DigitalPin.P13, 1)
+                pins.digitalWritePin(DigitalPin.P14, 0)
+            } else if (direction == 2) {
+                pins.digitalWritePin(DigitalPin.P13, 0)
+                pins.digitalWritePin(DigitalPin.P14, 1)
+            } else {
+                pins.digitalWritePin(DigitalPin.P13, 0)
+                pins.digitalWritePin(DigitalPin.P14, 0)
+            }
+        }
+        if (index == 3) {	//Motor3
+            if (direction == 1) {
+                pins.digitalWritePin(DigitalPin.P15, 1)
+                pins.digitalWritePin(DigitalPin.P16, 0)
+            } else if (direction == 2) {
+                pins.digitalWritePin(DigitalPin.P15, 0)
+                pins.digitalWritePin(DigitalPin.P16, 1)
+            } else {
+                pins.digitalWritePin(DigitalPin.P15, 0)
+                pins.digitalWritePin(DigitalPin.P16, 0)
+            }
+        }
+        if (index == 4) {	//Motor4
+            if (direction == 1) {
+                pins.digitalWritePin(DigitalPin.P1, 1)
+                pins.digitalWritePin(DigitalPin.P2, 0)
+            } else if (direction == 2) {
+                pins.digitalWritePin(DigitalPin.P1, 0)
+                pins.digitalWritePin(DigitalPin.P2, 1)
+            } else {
+                pins.digitalWritePin(DigitalPin.P1, 0)
+                pins.digitalWritePin(DigitalPin.P2, 0)
+            }
+        }
+    }
+}

4198e99802598e5f5e5d28ddd5b78ce4e5b05868
kukirobotclub <50830118+kukirobotclub@users.noreply.github.com>
2019-05-26 13:41:03 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//% weight=50 color=#ADB367 icon="\f085" block="KRC"
+//% weight=50 color=#ADB367 icon="\f013" block="モーター"
 namespace KRCmotor {
     /**
      * The user selects the 4-way dc motor.
